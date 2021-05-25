d7bdac5fc1f41e21a5a513fb895314bf84b1179e
Marcus Sanchez <m4rkuz@gmail.com>
2021-05-25 16:04:39 -0500

    Yet again more boilerplate

---
+++ orders/src/main/java/pro/marcuss/springnetflix/playground/orders/OrdersApplication.java
@@ -0,0 +1,15 @@
+package pro.marcuss.springnetflix.playground.orders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class OrdersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrdersApplication.class, args);
+	}
+
+}
